d2e7a90abcdc58b5ece79542341e0193ee4d28e3
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-09-06 02:46:23 -0500

    Base spring boot

---
+++ src/test/java/online/akcstudio/tareas/TareasApplicationTests.java
@@ -0,0 +1,13 @@
+package online.akcstudio.tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
